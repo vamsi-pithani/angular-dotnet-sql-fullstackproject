df8ecc86b2d0d8483779300e58d63a7372830aa9
vamsi <vamsi-pithani@outlook.com>
2025-07-19 18:55:56 +0530

    new25

---
+++ dotnet-api-temp/Migrations/20250719120000_AddPasswordToUsers.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_api_temp.Migrations
+{
+    public partial class AddPasswordToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
