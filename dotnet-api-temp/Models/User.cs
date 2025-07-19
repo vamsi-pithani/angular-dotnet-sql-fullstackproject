aaf85b3da3af5d9171269f8a3b1cc786f15a66a0
vamsi <vamsi-pithani@outlook.com>
2025-07-19 18:06:03 +0530

    new2

--- dotnet-api-temp/Models/User.cs
+++ dotnet-api-temp/Models/User.cs
@@ -3,8 +3,8 @@ namespace DotnetApi.Models
     public class User
     {
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
-        // Add other properties as needed
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
 }
